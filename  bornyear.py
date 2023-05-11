3d7153e8abb64483e0c383efc6e972744a2f1a1a
den287 <denis-v-ovchinnikov@yandex.ru>
2023-05-11 22:38:13 +0300

    Initial commit

---
+++  bornyear.py
@@ -0,0 +1,5 @@
+year_born = int(input('Напишите год рождения А. С. Пушкина: ', ))
+if year_born == 1799:
+    print('Верно')
+else:
+    print('Неверно')
---
+++ variables.py
@@ -0,0 +1,9 @@
+# Выберем объект для описания морковь и создадим несколоько переменных для ее описания:
+name_c = 'морковь'             # Название (имя)
+things_c = 6                   # Количество штук
+weight_kg = 0.858              # Вес
+qualitative_good = True        # Хорошее качество (да)
+print(type(name_c))            # Выведем типы переменных
+print(type(things_c))
+print(type(weight_kg))
+print(type(qualitative_good))
